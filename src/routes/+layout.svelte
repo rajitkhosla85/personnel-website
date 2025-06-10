0d66f4a69541d70cf61f983424fb1f9cdfeda65a
Rajit Khosla <rajitkhosla85@gmail.com>
2025-06-10 21:08:11 +0200

    fix: hamburger icon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -16,7 +16,17 @@
 	import type { PageData } from './$types';
 	import ContactModal from '$lib/components/ContactModal.svelte';
 
-	let { children, data }: { data: PageData; children: any } = $props();
+	let { children }: { data: PageData; children: any } = $props();
+	type Nav = {
+		displayName: string;
+		path: string;
+	};
+	const navs: Array<Nav> = [
+		{ displayName: 'HOME', path: 'home' },
+		{ displayName: 'PROJECTS', path: 'projects' },
+		{ displayName: 'BLOGS', path: 'blogs' }
+	];
+	const data = { navs };
 	console.log('data', data);
 	let openContact = $state(false);
 </script>
